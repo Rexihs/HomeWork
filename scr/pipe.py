e9d4f9e828bfd189436ca441ee85a8ba105d994c
Rexihs <fedoreev.4@gmail.com>
2026-04-23 22:55:22 +0300

    Исправлен расчёт забойного давления по устьевому, т.к. дарси-вейсбоха это не линейная функция пересчёт в обратном направлении не допустим. Решение с помощью fslove

--- scr/pipe.py
+++ scr/pipe.py
@@ -21,91 +21,75 @@ class Pipe:
         self.roughness = roughness
         self.vertical_depth = vertical_depth
         self.fluid = fluid
-
-    def dp(self, THP, q_std):
+    
+    def pwf_to_wh(self, P_bhp, q_std):
         """
-        Возращает потери давления по системе
-        
-        Параметры
-        ----------
-        THP [атм] - давление
-        q_std [ст. м3/сут] - объёмный расход газа в ст. условиях
+        Расчёт устьевого давления по Дарси–Вейсбаху
+        интегрирование BHP to THP
+
+        P_bhp [атм] - забойное давление
+        q_std [ст.м3/сут]
         """
+        P = float(P_bhp)
 
-        # FIXME: По хорошему, для дарси-вейсбаха разбить вертикальную скважину 
-        # на участки по 100 метров и производить расчёт для каждой части
-        # Добавить разбиение по vertical_dept если это не скважина вертикальная составляющая другая
-
-        # Реализация с разбиением на участки по 100 метров
-
-        Pi = THP # атм Первая точка, давление = Руст
-        dl = 100  # шаг по длине, м
-        n_steps = int(self.L // dl) # кол-во шагов
-
-        # Если кол-во шагов не хватает для длины скважины
+        dl = 100
+        n_steps = int(self.L // dl)
         if self.L % dl != 0:
             n_steps += 1
 
         for i in range(n_steps):
 
-            # корректная длина последнего участка, м
+            # длина участка
             if i == n_steps - 1:
                 dl_i = self.L - dl * (n_steps - 1)
             else:
                 dl_i = dl
 
-            # вертикальная составляющая, м
+            # вертикальная составляющая
             dz = dl_i * (self.vertical_depth / self.L)
 
-            # свойства газа при давлении в точке Pi
-            ro = self.fluid.get_ro(Pi) # кг/м3
-            Bg = self.fluid.get_bg(Pi) # дол.ед.
-            mu = self.fluid.get_mu(Pi) * 1e-3 # Па*с
+            # свойства при текущем давлении (ВАЖНО!)
+            ro = self.fluid.get_ro(P)
+            Bg = self.fluid.get_bg(P)
+            mu = self.fluid.get_mu(P) * 1e-3
 
-            u = q_std / 86400 * Bg / (np.pi * self.D**2 / 4) # м/с
+            # скорость
+            A = np.pi * self.D**2 / 4
+            u = q_std / 86400 * Bg / A
 
             Re = ro * u * self.D / mu
 
-            if Re < 2300: # Пуазейля
+            # коэффициент трения
+            if Re < 2300:
                 lyambda = 64 / Re
-            else: # Колбрука–Уайта
+            else:
                 def colebrook_eq(lmbd):
                     return 1/np.sqrt(lmbd) + 2*np.log10(
                         self.roughness/(3.7*self.D) + 2.51/(Re*np.sqrt(lmbd))
                     )
-
+                
                 lyambda = scipy.optimize.fsolve(colebrook_eq, 0.02)[0]
-    
+
             # потери давления
-            print(lyambda)
-            dp_fric = lyambda * (dl_i / self.D) * (ro * u**2 / 2) # На трение
-            dp_grav = ro * 9.80665 * dz # Гравитационная составляющая
+            dp_fric = lyambda * (dl_i / self.D) * (ro * u**2 / 2)
+            dp_grav = ro * 9.80665 * dz
+
             dp_i = (dp_fric + dp_grav) / 101325  # атм
-    
-            # Давление следующей точке Pi+1
-            Pi += dp_i
-            # print(f'Iteration {i}: Pi = {Pi}')
 
-        return (Pi - THP)
+            P -= dp_i
+            P = float(P)
 
-        # # Без разбиения на участки, запасной вариант
-        # ro = self.fluid.get_ro(THP) # кг/м3
-        # u = q_std/86400 * self.fluid.get_bg(THP) / (np.pi*self.D**2 / 4) # м/с
+        return P  # THP
 
-        # Re = ro*u*self.D / (self.fluid.get_mu(THP)*10**-3)
+    def dp(self, THP, q_std):
 
-        # if Re < 2300:
-        #     # Формула Пуазейля
-        #     lyambda = 64/Re
-        # else:
-        #     def colebrook_eq(lmbd):
-        #         return 1/np.sqrt(lmbd) + 2*np.log10(
-        #             self.roughness/(3.7*self.D) + 2.51/(Re*np.sqrt(lmbd))
-        #         )
+        def func(P_bhp):
+            P_bhp = float(P_bhp)
+            P_wh_calc = self.pwf_to_wh(P_bhp, q_std)
+            return P_wh_calc - THP
 
-        #     lyambda = scipy.optimize.fsolve(colebrook_eq, 0.02)[0]
+        P_bhp_guess = THP + 50  # старт
 
-        # # Дарси-Вейсбаха dp в СИ [Па]
-        # dp = lyambda * (self.L/self.D) * ((ro*u**2)/2) + ro*9.80665*self.vertical_depth
+        P_bhp = scipy.optimize.fsolve(func, P_bhp_guess)[0]
 
-        # return dp/101325
+        return P_bhp - THP
