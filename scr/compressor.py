32706627d84dfc6c20de6aa5c67935c7c60c4938
Rexihs <fedoreev.4@gmail.com>
2026-05-09 01:47:12 +0300

    Изменён учёт расхода газа в функции P_out, ещё не реализована зависимость от Q

--- scr/compressor.py
+++ scr/compressor.py
@@ -38,7 +38,8 @@ class DCS:
         на входе равно давлению в магистрали.
         """
 
-        Q_total = Q + self.q_ext
+        # Q_total = Q + self.q_ext # Учитываем в Q при вызове функции
+        # TODO: попробовать написать функцию P_out учитывающую расход газа
 
         if self.CR == 1.0:
             return self.P_line
